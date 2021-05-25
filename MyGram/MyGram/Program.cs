8e092b7ba67194929784e0662146e7c83fd62675
Rafael Hernandez <rhernandez7071@southwestuniversity.edu>
2021-05-25 10:47:53 -0600

    This is my first assignment

---
+++ MyGram/MyGram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyGram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintText();
+        }
+
+
+        public static void PrintText()
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Good evening");
+            Console.WriteLine("Good night");
+        }
+
+    }
+}
